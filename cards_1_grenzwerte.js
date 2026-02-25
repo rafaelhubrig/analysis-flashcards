f661feda61c1d29c00e109a2a84aeba628e20b61
rafaelhubrig <64662095+rafaelhubrig@users.noreply.github.com>
2026-02-25 21:22:11 +0100

    Update cards_1_grenzwerte.js

--- cards_1_grenzwerte.js
+++ cards_1_grenzwerte.js
@@ -1,4 +1,4 @@
-const cards_1_grenzwerte = [
+window.cards_1_grenzwerte = [
     { 
         q: "Definiere den Grenzwert einer Folge $(a_n)$ gegen $a$.", 
         a: "Eine Folge $(a_n)$ konvergiert gegen $a \\in \\mathbb{R}$, wenn gilt:<br>$\\forall \\varepsilon > 0 \\, \\exists n_0 \\in \\mathbb{N} \\, \\forall n \\ge n_0: |a_n - a| < \\varepsilon$." 
